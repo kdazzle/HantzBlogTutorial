931ccbe9e4485e51df152f65e54c5aa456486d1d
Kyle Valade <kvalade@carhartt.com>
2017-04-24 08:58:58 -0400

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Post(models.Model):
+
+    date_published = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=64)
---
+++ posts/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from posts.models import Post
+
+
+def index(request):
+    template = loader.get_template('posts/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return HttpResponse(template.render(context, request))
