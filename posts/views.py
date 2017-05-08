681387fe453d440f1bfe0259800b8b86fd1a8fa1
Kyle Valade <kvalade@carhartt.com>
2017-05-08 15:47:04 -0400

    Pretend the blog never existed

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
-from posts.models import Post
 
 
 def index(request):
     template = loader.get_template('posts/index.html')
-    posts = Post.objects.all()
-    context = {
-        'posts': posts,
-    }
+    context = {}
     return HttpResponse(template.render(context, request))
