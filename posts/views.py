9cd1f9b7353f45be8f8690f09d1cd99f1778f4c4
Kyle Valade <kvalade@carhartt.com>
2017-05-15 12:27:09 -0400

    Re-add posts to the index context

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Post
+
 
 def index(request):
     template = loader.get_template('posts/index.html')
-    context = {}
+    context = {
+        'posts': Post.objects.all(),
+    }
     return HttpResponse(template.render(context, request))
